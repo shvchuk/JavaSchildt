dbf057959608b39be843e902af535157d5e23d87
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2016-12-12 14:44:17 +0100

    vehicle constructor

--- src/Chapter4/Vehicle.java
+++ src/Chapter4/Vehicle.java
@@ -5,6 +5,13 @@ public class Vehicle {
     int fuelcap;
     int mpg;
 
+    // this is a constructor for Vehicle
+    Vehicle(int p, int f, int m){
+        passengers = p;
+        fuelcap = f;
+        mpg = m;
+    }
+
 //    void range(){ // method within the Vehicle class
 //        System.out.println("Range is " + fuelcap * mpg);
 //    }
